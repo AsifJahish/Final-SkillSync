edccf1972e5f4f5633cda12358be32753abd73ec
AsifJahish <asifjahish1@gmail.com>
2023-12-16 21:47:58 +0600

    last touch

---
+++ app/src/main/java/com/example/finalskillsync/Firebase/Models/Notes.kt
@@ -0,0 +1,16 @@
+package com.example.finalskillsync.Firebase.Models
+
+import kotlin.random.Random
+
+data class Notes (
+    var noteId: Long = Notes.generateRandomnoteId(),
+    var title: String? = null,
+    var detial: String? = null,
+    var userId: Long?= null,
+) {
+    companion object {
+        private fun generateRandomnoteId(): Long {
+            return Random.nextLong(1000000L, 9999999L)
+        }
+    }
+}
--- app/src/main/java/com/example/finalskillsync/Fragment/NoteInsertFragment.kt
+++ app/src/main/java/com/example/finalskillsync/Fragment/NoteInsertFragment.kt
@@ -5,19 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import com.example.finalskillsync.R
-import com.example.finalskillsync.databinding.FragmentFavoriteDetialBinding
 import com.example.finalskillsync.databinding.FragmentNoteBinding
+import com.example.finalskillsync.databinding.FragmentNoteInsertBinding
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
-
-class NoteFragment : Fragment() {
-    private var _binding: FragmentNoteBinding? = null
+class NoteInsertFragment : BottomSheetDialogFragment() {
+    private var _binding: FragmentNoteInsertBinding? = null
     private val binding get() = _binding!!
     companion object {
 
         fun newInstance() =
-            NoteFragment().apply {
+            NoteInsertFragment().apply {
 
             }
     }
@@ -25,21 +24,14 @@ class NoteFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentNoteBinding.inflate(inflater, container, false)
+        _binding = FragmentNoteInsertBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.addBtn.setOnClickListener {
-            val fragment = NoteInsertFragment()
-            fragment.show(requireActivity().supportFragmentManager, fragment.tag)
         }
 
     }
 
-
-
-
-}
